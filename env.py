0bf68efd3942327a879b3f8ed18da64aec3375a8
HLuke93 <lukehickson2020@gmail.com>
2022-11-04 14:53:42 +0000

    deploy to heroku

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ['DATABASE_URL'] = 'postgres://bhwveudbldanvk:335506f67cb7551763df42a188fac9fc911827e4df86d453135af0bbad5320c2@ec2-54-75-184-144.eu-west-1.compute.amazonaws.com:5432/dc4lpoaaflrje4'
+os.environ['SECRET_KEY'] = '123456789'
+
+os.environ['CLOUDINARY_URL'] = 'cloudinary://857947124835799:acbcNRBSvjOk2lMCONuBJpoWAfI@duiuraqzo'
