36dfe6469bcefa99e282c3b275f6f5e0dd7ce184
HLuke93 <lukehickson2020@gmail.com>
2022-11-07 12:43:57 +0000

    add extra functionality to admin panel , add comment model to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from .models import Post
-from django_summernote.admin import SummerNoteModelAdmin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
-class PostAdmin(SummerNoteModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+
+
+
+
 
