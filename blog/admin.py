64ff43bfcbb60697d4f1bdded9ef05cfe43d1fc1
HLuke93 <lukehickson2020@gmail.com>
2022-11-07 12:31:09 +0000

    installed summernote , and added summernote to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummerNoteModelAdmin
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(SummerNoteModelAdmin):
+    summernote_fields = ('content')
+
